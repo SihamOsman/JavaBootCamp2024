475f8ef4f00b759fa42c5069a216d81ff2237a0b
Siham Osman (PRIME 8 LLC) <v-osmansiham@microsoft.com>
2024-08-28 19:43:40 -0700

    commit message

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+printAreaResult();
+        }
+    public static void  areaCircle() {
+        double radius=28;
+        double diameter=3;
+        System.out.println(radius*diameter);
+    }
+    public static void printAreaResult() {
+        areaCircle();
+    }
+    }
